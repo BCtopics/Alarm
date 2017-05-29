3f573d65712f0276b5c70ae3b82b2e4cd8a91617
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-29 16:41:35 -0600

    Initial Custom View Cell, Setup Views/Custom cells

---
+++ Alarm/Alarm/SwitchTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Bradley GIlmore on 5/29/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: NSLayoutConstraint!
+    
+    //MARK: - IBActions
+    
+    @IBAction func switchValueChanged(_ sender: Any) {
+        
+        
+        
+    }
+    
+
+
+}
