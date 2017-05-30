73f20529d0699a4099a3b298edca242a3ba4c261
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-30 13:45:43 -0600

    Implement Model/Helper

---
+++ Alarm/Alarm/DateHelper.swift
@@ -0,0 +1,25 @@
+//
+//  DateHelper.swift
+//  Alarm
+//
+//  Created by Bradley GIlmore on 5/30/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import Foundation
+
+enum DateHelper {
+    
+    static var thisMorningAtMidnight: Date? {
+        let calendar = Calendar.current
+        let now = Date()
+        return calendar.date(bySettingHour: 0, minute: 0, second: 0, of: now)
+    }
+    
+    static var tomorrowMorningAtMidnight: Date? {
+        let calendar = Calendar.current
+        guard let thisMorningAtMidnight = thisMorningAtMidnight else { return nil }
+        return calendar.date(byAdding: .day, value: 1, to: thisMorningAtMidnight)
+    }
+}
+
